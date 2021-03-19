8e8269da34571bcf153f9d2e2a98a8257db8eda4
evertonesc <evertonsouzaesc@gmail.com>
2021-03-18 22:29:28 -0300

    Test: Unit tests fixtures and configuration

---
+++ backend/Hw.Api/tests/Unit/Hw.Api.Tests.Unit/Config/UnitTestConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Hw.Api.Tests.Unit.Entities.Interfaces;
+using Microsoft.Extensions.Configuration;
+
+namespace Hw.Api.Tests.Unit.Config
+{
+    public class UnitTestConfiguration : IUnitTestConfiguration
+    {
+        public IConfiguration GetConfig() =>
+
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.Tests.json", optional: false, reloadOnChange: true)
+                .Build();
+    }
+}
---
+++ backend/Hw.Api/tests/Unit/Hw.Api.Tests.Unit/Config/UnitTestsFixture.cs
@@ -0,0 +1,18 @@
+﻿using Hw.Api.Tests.Unit.Entities.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Hw.Api.Tests.Unit.Config
+{
+    public class UnitTestsFixture
+    {
+        public ServiceProvider ServiceProvider { get; set; }
+
+        public UnitTestsFixture()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddTransient<IUnitTestConfiguration, UnitTestConfiguration>();
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+    }
+}
