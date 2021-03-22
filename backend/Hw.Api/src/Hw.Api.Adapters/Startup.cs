f33b8c3faaae142d72ee1ec2d4f949235d185466
evertonesc <evertonsouzaesc@gmail.com>
2021-03-21 22:12:36 -0300

    Feat: Register database connection

--- backend/Hw.Api/src/Hw.Api.Adapters/Startup.cs
+++ backend/Hw.Api/src/Hw.Api.Adapters/Startup.cs
@@ -1,15 +1,9 @@
+using Hw.Api.Infrastructure.Database.Core;
+using Hw.Api.Ioc;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Hw.Api
 {
@@ -26,16 +20,13 @@ namespace Hw.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.ConnectMongoDb(Configuration);
+            services.DependecyManager();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseHttpsRedirection();
 
             app.UseRouting();
