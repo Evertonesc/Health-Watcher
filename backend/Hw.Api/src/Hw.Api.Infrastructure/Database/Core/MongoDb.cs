a4ac3a7c316be78c227d1732786b8c0b2186a84d
evertonesc <evertonsouzaesc@gmail.com>
2021-03-21 22:13:14 -0300

    Feat: Mongo db connection implementation

---
+++ backend/Hw.Api/src/Hw.Api.Infrastructure/Database/Core/MongoDb.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace Hw.Api.Infrastructure.Database.Core
+{
+    public static class MongoDb
+    {
+        public static void ConnectMongoDb(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mongoConnectionString = configuration.GetSection("MongoDb:ConnectionString").Value;
+            services.AddSingleton<IMongoClient>(new MongoClient(mongoConnectionString));
+        }
+    }
+}
